f24a12610582e50b9a8ba9218a8fef0813ed2e7f
npoputnikov <npoputnikov@any.run>
2026-01-01 05:59:30 +0300

    fix(build): Force add embedded driver stubs

---
+++ cheats/cs2/src/embedded_driver_data.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace embedded {
+    // Stub driver data (empty)
+    // Use bin2header.py to generate real data from iqvw64e.sys
+    const unsigned char driver_data[] = { 0x00 };
+    const size_t driver_size = 0;
+}
+
